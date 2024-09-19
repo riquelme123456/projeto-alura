2eaccdefb69778f32532a064062eb6f2c931bf2a
riquelme123456 <deboramoreirajubileu@gmail.com>
2024-09-19 20:52:39 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function draw() {
+    background(220);
+  }function setup() {
+    createCanvas(600, 600);
+    background("black")
+  }
+  
+  function draw() {
+     stroke("blue");
+     fill("red");
+  
+    if(mouseIsPressed){
+       rect(mouseX, mouseY, 29, 35);
+    }
+  
+   }
